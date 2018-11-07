99bbd7b6cf71991ffabc83a406dbde06d2cf3ef0
Dinuka Arseculeratne <dinukaarseculeratne@Dinukas-MacBook-Pro.local>
2018-11-07 15:36:24 +1100

    initial commit

---
+++ src/main/java/com/justiceleague/justiceleaguetracker/JusticeleagueTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.justiceleague.justiceleaguetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JusticeleagueTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JusticeleagueTrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/justiceleague/justiceleaguetracker/JusticeleagueTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.justiceleague.justiceleaguetracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JusticeleagueTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
